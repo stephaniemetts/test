4fa1d3c52d3b0ba5fdde64f479a06ce0ae34e17b
saasbook <saasbook@saasbook.(none)>
2014-10-02 09:39:17 -0300

    start of new ch4 walkthrough

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatos::Application.config.secret_token = '8612e7771c34601a0acfe554ac0545c579bdf7f5a908fd90190564b7ef94a9f71bdd72ffc559170b61434e52192c85cbf0b4629000bf96ff78f97bd37695b34e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rottenpotatos::Application.config.session_store :cookie_store, key: '_rottenpotatos_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rottenpotatos::Application.config.session_store :active_record_store
